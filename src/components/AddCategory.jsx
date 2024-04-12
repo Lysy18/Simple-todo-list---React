db08f76001483f3badc68e746d74d311a045b44c
Lysy18 <taborbartlomiej@gmail.com>
2024-04-12 23:12:28 +0200

    addCategory

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,14 +1,5 @@
-// import { useState } from "react";
-import PropTypes from "prop-types";
 const AddCategory = ({ onShadowTest }) => {
-  // const handleAddCategory = () => {
-  //   // setIsAddCategory((addPosi) => !addPosi);
-  //   console.log(isAddCategory);
-  // };
 
-  const test = () => {
-    onShadowTest;
-  };
   return (
     <>
       <div className="addCategoryBtn">
@@ -18,8 +9,6 @@ const AddCategory = ({ onShadowTest }) => {
   );
 };
 
-// AddCategory.propTypes = {
-//   onAddCategory: PropTypes.func.isRequired,
-// };
+
 
 export default AddCategory;
